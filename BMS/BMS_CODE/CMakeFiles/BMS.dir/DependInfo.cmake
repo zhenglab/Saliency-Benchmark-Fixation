1bd28375e86a40ef0d131897439ae0d1d44f6bf5
zhuyafei <zhuyafei4520@163.com>
2016-04-13 20:08:15 +0800

    Update BMS

---
+++ BMS/BMS_CODE/CMakeFiles/BMS.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/BMS.cpp" "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/CMakeFiles/BMS.dir/BMS.cpp.o"
+  "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/BMS.h" "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/CMakeFiles/BMS.dir/BMS.h.o"
+  "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/OS_specific.h" "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/CMakeFiles/BMS.dir/OS_specific.h.o"
+  "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/fileGettor.h" "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/CMakeFiles/BMS.dir/fileGettor.h.o"
+  "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/main.cpp" "/Users/fei/repository/zhenglab/Saliency-Benchmark-Fixation/BMS/BMS_CODE/CMakeFiles/BMS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ BMS/BMS_CODE/CMakeFiles/BMS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BMS.dir/BMS.h.o"
+  "CMakeFiles/BMS.dir/fileGettor.h.o"
+  "CMakeFiles/BMS.dir/OS_specific.h.o"
+  "CMakeFiles/BMS.dir/BMS.cpp.o"
+  "CMakeFiles/BMS.dir/main.cpp.o"
+  "BMS.pdb"
+  "BMS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
